9eaab03efd723403d582d5654664bb1033025379
Theo Mironidis <mironidis.gr@gmail.com>
2022-10-26 21:52:06 +0100

    LinkedList: complete Singly Linked List

--- src/LinkedList/LinkedList.test.ts
+++ src/LinkedList/LinkedList.test.ts
@@ -1,9 +1,8 @@
-import { createListNode } from "./LinkedList";
+import { LinkedList, createListNode } from "./LinkedList";
 
 describe("Linked List", () => {
-  it("should let you create a Linked List", () => {
+  it("should let you link multiple List Nodes ", () => {
     const headNode = createListNode(5);
-
     const node = createListNode(6, headNode);
 
     expect(node).toEqual({
@@ -11,6 +10,24 @@ describe("Linked List", () => {
       next: {
         value: 5,
         next: null,
+      },
+    });
+  });
+
+  it("should create a Singly Linked List", () => {
+    const list = LinkedList(1).next(2).next(3).next("end");
+
+    expect(list.value).toEqual({
+      value: 1,
+      next: {
+        value: 2,
+        next: {
+          value: 3,
+          next: {
+            value: "end",
+            next: null,
+          },
+        },
       },
     });
   });
--- src/LinkedList/LinkedList.ts
+++ src/LinkedList/LinkedList.ts
@@ -7,3 +7,24 @@ export const createListNode = (value: any, next?: ListNode): ListNode => ({
   value,
   next: next || null,
 });
+
+export const LinkedList = (
+  initialValue: any
+): {
+  value: ListNode;
+  next: Function;
+} => {
+  const headNode = createListNode(initialValue);
+  let previousNode = headNode;
+
+  return {
+    value: headNode,
+    next: function (value: any) {
+      const node = createListNode(value);
+      previousNode.next = node;
+      previousNode = node;
+
+      return this;
+    },
+  };
+};
