d33e9f54aeb30981bcf00c1a5a08f00a98e7f805
Theo Mironidis <mironidis.gr@gmail.com>
2022-11-07 23:23:34 +0000

    LinkedList: insert after

--- src/dataStructures/LinkedList/LinkedList.test.ts
+++ src/dataStructures/LinkedList/LinkedList.test.ts
@@ -16,13 +16,13 @@ describe("Linked List", () => {
 
   it("should create a Singly Linked List", () => {
     const list = LinkedList(1)
-      .next(2)
-      .next(3)
-      .next("end")
-      .head("start")
-      .next("this is the end");
+      .insert(2)
+      .insert(3)
+      .insert("end")
+      .insertHead("start")
+      .insert("this is the end");
 
-    expect(list.value).toEqual({
+    expect(list.head).toEqual({
       value: "start",
       next: {
         value: 1,
@@ -40,6 +40,54 @@ describe("Linked List", () => {
           },
         },
       },
+    });
+  });
+
+  describe("insert after", () => {
+    it("should create a Singly Linked List and insert after head node", () => {
+      const list = LinkedList(1).insert(3).insert(4).insert(5);
+
+      list.insertAfter(list.head, 2);
+
+      expect(list.head).toEqual({
+        value: 1,
+        next: {
+          value: 2,
+          next: {
+            value: 3,
+            next: {
+              value: 4,
+              next: {
+                value: 5,
+                next: null,
+              },
+            },
+          },
+        },
+      });
+    });
+
+    it("should create a Singly Linked List and insert after 4", () => {
+      const list = LinkedList(1).insert(3).insert(4).insert(5);
+
+      list.insertAfter(list.head.next.next, 400);
+
+      expect(list.head).toEqual({
+        value: 1,
+        next: {
+          value: 3,
+          next: {
+            value: 4,
+            next: {
+              value: 400,
+              next: {
+                value: 5,
+                next: null,
+              },
+            },
+          },
+        },
+      });
     });
   });
 });
--- src/dataStructures/LinkedList/LinkedList.ts
+++ src/dataStructures/LinkedList/LinkedList.ts
@@ -11,27 +11,51 @@ export const createListNode = (value: any, next?: ListNode): ListNode => ({
 export const LinkedList = (
   initialValue: any
 ): {
-  value: ListNode;
-  next: Function;
-  head: Function;
+  head: ListNode;
+  insert: Function;
+  insertHead: Function;
+  insertAfter: Function;
+  findMatchingNode: Function;
 } => {
   const headNode = createListNode(initialValue);
   let leafNode = headNode;
 
   return {
-    value: headNode,
-    next: function (value: any) {
+    head: headNode,
+    insert: function (value: any) {
       const node = createListNode(value);
       leafNode.next = node;
       leafNode = node;
 
       return this;
     },
-    head: function (value: any) {
-      const node = createListNode(value, this.value);
-      this.value = node;
+    insertHead: function (value: any) {
+      const node = createListNode(value, this.head);
+      this.head = node;
 
       return this;
+    },
+    findMatchingNode: function (
+      listNode: ListNode,
+      startingNode: ListNode = this.head
+    ) {
+      if (startingNode.next === null) {
+        return undefined;
+      }
+      if (startingNode === listNode) {
+        return startingNode;
+      }
+      return this.findMatchingNode(listNode, startingNode.next);
+    },
+    insertAfter: function (listNode: ListNode, value: any) {
+      let whichNode = this.findMatchingNode(listNode);
+      if (whichNode) {
+        const newNode = createListNode(value, whichNode.next);
+        whichNode.next = newNode;
+        return this;
+      }
+
+      throw new Error("The node you passed was not found 🤷‍♂️");
     },
   };
 };
